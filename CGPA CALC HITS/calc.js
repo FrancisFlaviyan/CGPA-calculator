2f58b265e0418571d8d5ef2ed8ae4393e306eac5
FrancisFlaviyan <francisflaviyan@gmail.com>
2021-09-09 22:30:00 +0530

    calc.js updated

--- CGPA CALC HITS/calc.js
+++ CGPA CALC HITS/calc.js
@@ -1,55 +1,32 @@
 console.log("calc.js loaded!");
 
 let res = [];
-let gpa=0;
-const setValue = (id, className, credit) => { //eg: id-grade1 className: gradeA
-    let tempRes = document.querySelector(`.${className}`).value*credit;
-    let obj = {
-        id,
-        grade: tempRes
+
+let gpa=[];
+
+const setValue = (index, id, credit) => { 
+     
+    res[index] =  document.querySelector(`#${id}`).value*credit;
+    console.log(id +"-"+ res[index]);   
+};
+
+const calc=()=>{
+
+    for(i=0;i<8;i++){
+        gpa[i]=0;
     };
-    res[id] = obj;
-    console.log(id +"-"+ res[id].grade);
 
-    // for(i=0;i<res.length;i++) { res[i].grade}
+    for(i=0;i<7;i++){
+        gpa[0]+=res[i]
+    };
 
-  
-    if (res.length > 0) {
-        for (i = 0; i < res.length; i++) {
-            if (res[i].grade) {
-                gpa += res[i].grade
-            }
-        };
-        gpa /= 22;
-        gpa = gpa.toFixed(2);
-        console.log("your gpa is" + gpa);
-        // alert("your gpa is  " + gpa);
-    }
-
-    // if(res.length>0){
-
-    //     for(i=0;i<res.length;i++){
-        
-    //         gpa+=res[i].grade};
-
-    //         gpa/=22;
-    //         gpa=gpa.toFixed(2);
-        
-       
-        
-    //     console.log("your gpa is"+gpa);
-    //     // alert("your gpa is  "+gpa);
-
-    //     if(res[i].grade){gpa+=res[i].grade}
+    gpa[0]/=21.5;
+    gpa[0]=gpa[0].toFixed(2);
 
 
-        
-    // }
     
-    // console.table(res);
-    // console.log(res[1]);
-    // console.log(res[1].id);
-    // console.log(res[1].grade);
+alert(gpa[0]);
+
 };
 
 
@@ -67,31 +44,3 @@ const setValue = (id, className, credit) => { //eg: id-grade1 className: gradeA
 
 
 
-// function getvalue(){
-//     var x=document.querySelector("select[1]");
-//     // console.log("working fun");
-//     // console.log(x);
-//     // console.log(x.id);
-//     console.log(x.className);
-//     x="";
-
-//     if (x.classname==="five-cred"){
-//         console.log("working if");
-
-// var grade = document.querySelector(".five-cred");
-// var res = grade.value;
-// // console.log("grade score="+res+"   credit score="+res*5);
-// console.log(res);
-//     }
-
-
-//     else if (x.classname==="four-cred") {
-
-//         var grade = document.querySelector(".four-cred");
-//         var res = grade.value;
-//         // console.log("grade score="+res+"   credit score="+res*5);
-//         console.log(res);
-//             }
-
-
-// };
