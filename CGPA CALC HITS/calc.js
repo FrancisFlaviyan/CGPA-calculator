df6c0b1576169c015469ef55765bae993da78f17
FrancisFlaviyan <francisflaviyan@gmail.com>
2021-09-10 20:34:06 +0530

    calc.js updated

--- CGPA CALC HITS/calc.js
+++ CGPA CALC HITS/calc.js
@@ -4,6 +4,8 @@ let res = [];
 
 let gpa=[];
 
+let credits=[17.5,24.5,22,22,23,25,23,8];
+
 
 
 const setValue = (index, id, credit) => { 
@@ -27,57 +29,57 @@ const calc=()=>{
     for(i=0;i<7;i++){
         gpa[0]+=res[i]
     };
-    gpa[0]/=21.5;
+    gpa[0]/=credits[0];
 
 
     // sem 2 gpa
     for(i=7;i<17;i++){
         gpa[1]+=res[i]
     };
-    gpa[1]/=21.5; 
+    gpa[1]/=credits[1]; 
 
 
     // sem 3 gpa
     for(i=0;i<7;i++){
         gpa[2]+=res[i]
     };
-    gpa[2]/=21.5;
+    gpa[2]/=credits[2];
 
 
     // sem 4 gpa
     for(i=0;i<7;i++){
         gpa[3]+=res[i]
     };
-    gpa[3]/=21.5;
+    gpa[3]/=credits[3];
 
-    
+
 
     // sem 5 gpa
     for(i=0;i<7;i++){
         gpa[0]+=res[i]
     };
-    gpa[0]/=21.5;
+    gpa[4]/=credits[4];
 
 
     // sem 6 gpa
     for(i=0;i<7;i++){
         gpa[0]+=res[i]
     };
-    gpa[0]/=21.5;
+    gpa[5]/=credits[5];
 
 
     // sem 7 gpa
     for(i=0;i<7;i++){
         gpa[0]+=res[i]
     };
-    gpa[0]/=21.5;
+    gpa[6]/=credits[6];
 
 
     // sem 8 gpa
     for(i=0;i<7;i++){
         gpa[0]+=res[i]
     };
-    gpa[0]/=21.5;
+    gpa[7]/=credits[7];
 
     // Decimal places
     for(i=0;i<8;i++){
