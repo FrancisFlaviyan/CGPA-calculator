0de2c40ed41b5214b4d6e85b2b23607aefed31f0
FrancisFlaviyan <francisflaviyan@gmail.com>
2021-09-13 15:54:30 +0530

    calc.js updated

--- CGPA CALC HITS/calc.js
+++ CGPA CALC HITS/calc.js
@@ -2,6 +2,13 @@ console.log("calc.js loaded!");
 
 let res = [];
 
+let cgpa=0;
+
+let cgpaNum=0;
+let cgpaDen=0;
+
+      
+
 let gpa=[];
 
 let credits=[17.5,24.5,22,22,23,25,23,8];
@@ -10,6 +17,7 @@ let credits=[17.5,24.5,22,22,23,25,23,8];
 
 
 const setValue = (index, id, credit) => { 
+
      
     res[index] =  document.querySelector(`#${id}`).value*credit;
     console.log(id +"-"+ res[index]);   
@@ -23,6 +31,8 @@ const calc=()=>{
     for(i=0;i<8;i++){
         gpa[i]=0;
     };
+
+      
              //          ........               //
 
 
@@ -43,53 +53,49 @@ const calc=()=>{
     for(i=0;i<7;i++){
         gpa[2]+=res[i]
     };
-    gpa[2]/=credits[2];
+    
 
 
     // sem 4 gpa
     for(i=0;i<7;i++){
         gpa[3]+=res[i]
     };
-    gpa[3]/=credits[3];
 
 
 
     // sem 5 gpa
     for(i=0;i<7;i++){
-        gpa[0]+=res[i]
+        gpa[4]+=res[i]
     };
-    gpa[4]/=credits[4];
+    
 
 
     // sem 6 gpa
     for(i=0;i<7;i++){
-        gpa[0]+=res[i]
+        gpa[5]+=res[i]
     };
-    gpa[5]/=credits[5];
+
 
 
     // sem 7 gpa
     for(i=0;i<7;i++){
-        gpa[0]+=res[i]
+        gpa[6]+=res[i]
     };
-    gpa[6]/=credits[6];
+    
 
 
     // sem 8 gpa
     for(i=0;i<7;i++){
-        gpa[0]+=res[i]
+        gpa[7]+=res[i]
     };
-    gpa[7]/=credits[7];
+    
 
 
     for(i=2;i<8;i++){
         gpa[i]=0;
     };
 
-    // Decimal places
-    // for(i=0;i<8;i++){
-    // gpa[i]=gpa[i].toFixed(2); }
-    //
+  
 
 
      //cgpa
@@ -116,14 +122,6 @@ var cgpaDen=0;
 
 
 
-
-
-//   if(isNaN(gpa[1])){
-//       alert("fuck off");
-//   }
-
-//   else{
-//     alert(gpa[1]);}
 
 };
 
